35348a389dfdc0871d10d96f7b1b92947e472cf2
Sloboda <alexnaos@yandex.ru>
2026-04-18 23:09:55 +0300

    111

--- .pio/build/esp32dev/lib54e/Settings/core/SettingsBase_class.cpp.d
+++
@@ -1,3 +0,0 @@
-.pio/build/esp32dev/lib54e/Settings/core/SettingsBase_class.cpp.o: \
- .pio/libdeps/esp32dev/Settings/src/core/SettingsBase_class.cpp \
- .pio/libdeps/esp32dev/Settings/src/core/SettingsBase_class.h
